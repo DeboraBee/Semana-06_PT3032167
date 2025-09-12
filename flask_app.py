3722c40ee61303ec27fa016d2a3afa08f8fdbe7f
Deborange <101590106+DeboraBee@users.noreply.github.com>
2025-09-11 21:34:55 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,25 +1,29 @@
 # A very simple Flask Hello World app for you to get started with...
-from flask import Flask, render_template, url_for, flash, session, redirect
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask import request 
+from flask import make_response
+from flask import url_for, flash, session, redirect
 from datetime import datetime
 from flask_wtf import Flaskform
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-app.config['SECRET KEY'] = 'Chave forte'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+app.config['SECRET KEY'] = 'Chave forte'
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?',  validators = [DataRequired()])
     submit = SubmitField('Submit')
 
-@app.route('/', methods=['GET','POST']),
+@app.route('/', methods=['GET','POST'])
 def index():
     form = NameForm()
-    if form_validate_on_sumbit():
+    if form.validate_on_sumbit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Look  like you have changed your name!')
