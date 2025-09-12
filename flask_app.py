37eb5eb27fd8e370451df9be89da614112e7bf4a
Deborange <101590106+DeboraBee@users.noreply.github.com>
2025-09-11 22:21:48 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-app.config['SECRET KEY'] = "Chave forte"
+app.config['SECRET_KEY'] = "Chave forte"
 
 class NameForm(FlaskForm):
     name = StringField("What's your name?",  validators = [DataRequired()])
